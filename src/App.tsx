6c702b365819bdf35265aced162a9d48b97f8dc1
SAMUEL DE OLIVEIRA AUGUSTO <samuel.augusto@etec.sp.gov.br>
2025-08-13 12:01:40 -0300

    feat: início do projeto + testes iniciais.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <div className="columns-3">
+        <div className="bg-red-500 p-4 m-2 text-white">Column 1</div>
+        <div className="bg-green-500 p-4 m-2 text-white">Column 2</div>
+        <div className="bg-blue-500 p-4 m-2 text-white">Column 3</div>
+      </div>
+    </>
+  )
+}
+
+export default App
